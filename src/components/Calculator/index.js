5adff41005e4afa1ca41d91b513d0f4a77f099d9
böris <boris.yarmoulnik@gmail.com>
2021-07-14 18:11:28 +0300

    feat: handling operation and equal button

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -2,9 +2,9 @@ import { useState } from "react";
 import { Button, Container, Current, Previous, Screen } from "../Styled";
 
 export default function Calculator () {
-  const [previous, setPrevious] = useState('');
-  const [current, setCurrent] = useState('');
-  const [operation, setOperation] = useState('');
+  const [previous, setPrevious] = useState("");
+  const [current, setCurrent] = useState("");
+  const [operation, setOperation] = useState("");
 
   const appendValue = (el) => {
     const value = el.target.getAttribute("data");
@@ -13,7 +13,6 @@ export default function Calculator () {
   }
 
   const handleDelete = () => {
-    console.log('1');
     setCurrent(String(current).slice(0, -1));
   }
 
@@ -21,6 +20,32 @@ export default function Calculator () {
     setPrevious("");
     setCurrent("");
     setOperation("");
+  }
+
+  const chooseOperation = (el) => {
+    if (current === "") return;
+
+    if (previous !== "") {
+      let value = compute();
+      setPrevious(value);
+    } else {
+      setPrevious(current);
+    }
+
+    setCurrent("");
+    setOperation(el.target.getAttribute("data"));
+  }
+
+  const equals = () => {
+    let value = compute();
+    if (value == undefined || value == null) return;
+
+    setCurrent(value);
+    setPrevious("");
+    setOperation("");
+  }
+
+  const compute = () => {
   }
 
   return (
@@ -32,26 +57,26 @@ export default function Calculator () {
 
       <Button onClick={handleAllClear} gridSpan={2} control>AC</Button>
       <Button onClick={handleDelete} ontrol>DEL</Button>
-      <Button operation>÷</Button>
+      <Button onClick={chooseOperation} data={"+"} operation>÷</Button>
 
-      <Button data={"7"} onClick={appendValue}>7</Button>
-      <Button data={"8"} onClick={appendValue}>8</Button>
-      <Button data={"9"} onClick={appendValue}>9</Button>
-      <Button operation>x</Button>
+      <Button onClick={appendValue} data={"7"}>7</Button>
+      <Button onClick={appendValue} data={"8"}>8</Button>
+      <Button onClick={appendValue} data={"9"}>9</Button>
+      <Button onClick={chooseOperation} data={"×"} operation>×</Button>
 
-      <Button data={"4"} onClick={appendValue}>4</Button>
-      <Button data={"5"} onClick={appendValue}>5</Button>
-      <Button data={"6"} onClick={appendValue}>6</Button>
-      <Button operation>+</Button>
+      <Button onClick={appendValue} data={"4"}>4</Button>
+      <Button onClick={appendValue} data={"5"}>5</Button>
+      <Button onClick={appendValue} data={"6"}>6</Button>
+      <Button onClick={chooseOperation} data={"+"} operation>+</Button>
 
-      <Button data={"1"} onClick={appendValue}>1</Button>
-      <Button data={"2"} onClick={appendValue}>2</Button>
-      <Button data={"3"} onClick={appendValue}>3</Button>
-      <Button operation>-</Button>
+      <Button onClick={appendValue} data={"1"}>1</Button>
+      <Button onClick={appendValue} data={"2"}>2</Button>
+      <Button onClick={appendValue} data={"3"}>3</Button>
+      <Button onClick={chooseOperation} data={"-"} operation>–</Button>
 
-      <Button data={"."} onClick={appendValue} period control>.</Button>
-      <Button data={"0"} onClick={appendValue}>0</Button>
-      <Button gridSpan={2} operation equals>=</Button>
+      <Button onClick={appendValue} data={"."} period control>.</Button>
+      <Button onClick={appendValue} data={"0"}>0</Button>
+      <Button onClick={equals} gridSpan={2} operation equals>=</Button>
     </Container>
   )
 }
